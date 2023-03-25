e07cc34bb3768ea8f6a6420893ca697365cd1dff
zhicong-zhang <zhicong.zhang@cnsdc01.pwc.com>
2023-03-25 21:34:09 +0800

    update

--- app.ts
+++ app.ts
@@ -6,26 +6,31 @@ const app = express();
 
 var port = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-    response.setHeader('content-type', 'text/html;charset=utf-8');
-    // let resMethod = request.method;
-    let{method} = request;
-    let {url} = request;
-
-    // 获取url路径
-    let {pathname} = new URL(request.url, 'http://127.0.0.1' || 'https://v2ray0.azurewebsites.net');
-    let filePath = __dirname + '/page' + pathname;
-    // 读取文件 fs 异步 api
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            response.statusCode = 500;
-            response.end('html文件读取失败')
-            return;
-        }
-        response.end(data);
-    });
-
+app.get('/', (req, res) => {
+    return res.send('hello!')
 })
-server.listen(port, () => {
-    console.log('启动成功...');
+app.listen(port, () => {return console.log('runnning with ' + port);
 })
+// const server = http.createServer((request, response) => {
+//     response.setHeader('content-type', 'text/html;charset=utf-8');
+//     // let resMethod = request.method;
+//     let{method} = request;
+//     let {url} = request;
+
+//     // 获取url路径
+//     let {pathname} = new URL(request.url, 'http://127.0.0.1' || 'https://v2ray0.azurewebsites.net');
+//     let filePath = __dirname + '/page' + pathname;
+//     // 读取文件 fs 异步 api
+//     fs.readFile(filePath, (err, data) => {
+//         if (err) {
+//             response.statusCode = 500;
+//             response.end('html文件读取失败')
+//             return;
+//         }
+//         response.end(data);
+//     });
+
+// })
+// server.listen(port, () => {
+//     console.log('启动成功...');
+// })
